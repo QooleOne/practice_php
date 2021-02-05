b60cc14ac62a9fdc43950b1793aaa09c55e16650
QooleOne <ashafiev@gmail.com>
2021-02-05 19:37:51 +0100

    test commit via Git Gui

--- logo.inc.php
+++ logo.inc.php
@@ -1,5 +1,5 @@
     
                 <div class="logo"> 
-                <a href="index.php"><img src="img/l.png" alt="php"></a>
+					<a href="index.php"><img src="img/l.png" alt="php"></a>
                 </div>                                                 
         
